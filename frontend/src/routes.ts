49a95ed4cf9d9c3f2565c1415d29679a8407d232
Hossein Heidari <squanderness@gmail.com>
2024-04-04 23:24:02 +0330

    rm junk

--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -1,13 +1,7 @@
-import App from "./App.vue"
 import OutfitGeneratorPage from "./pages/OutfitGeneratorPage.vue"
 import NotFound from "./pages/NotFound.vue"
 
 export const Routes = [
-  // {
-  //   path: "/",
-  //   name: "home",
-  //   component: App,
-  // },
   {
     path: "/",
     name: "outfitGenerator",
