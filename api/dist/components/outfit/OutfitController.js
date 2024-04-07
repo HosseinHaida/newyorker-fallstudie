4ee0782f82a265d3989e3b2d0ca0aadde6e4769e
Hossein Heidari <squanderness@gmail.com>
2024-04-07 16:59:54 +0330

    removed /dist from git

--- api/dist/components/outfit/OutfitController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const OutfitControllerFetchRandom_1 = __importDefault(require("./OutfitControllerFetchRandom"));
-exports.default = {
-    fetchRandom: OutfitControllerFetchRandom_1.default,
-};
--- api/dist/helpers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getRandomItem = void 0;
-const getRandomItem = (arr) => {
-    const randomIndex = Math.floor(Math.random() * arr.length);
-    return arr[randomIndex];
-};
-exports.getRandomItem = getRandomItem;
